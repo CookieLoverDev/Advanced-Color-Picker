0e51b4af9375a4c163572c95f4efc0768653f9cb
CookieLoverDev <ukhodjamov@gmail.com>
2024-10-29 14:18:07 +0100

    Initial Commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AdvancedColorPicker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void ChangeColor(object sender, RoutedPropertyChangedEventArgs<Color?> e)
+        {
+            colorName.Text = $"Current color: {e.NewValue}";
+            this.Background = new SolidColorBrush(e.NewValue ?? Colors.White);
+        }
+    }
+}
